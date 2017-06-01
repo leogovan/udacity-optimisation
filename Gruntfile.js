f0ac1efca74251d3baca6cfcd7731707cc52c8ab
Leofric Govan <leogovan@Leos-MacBook-Pro-2.local>
2017-06-01 13:09:26 +0100

    Update gruntfile with config, minify css and inline

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,8 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		
+		// ------- Page Speed Config ------- //
 		pagespeed: {
 			options: {
 				nokey: true,
@@ -24,19 +26,42 @@ module.exports = function(grunt) {
 					threshold: 90
 				}
 			}
+		},
+
+		// ------- CSS Minify Config ------- //
+
+		cssmin: {
+			target: {
+				files: [{
+					expand: true,
+					cwd: 'css',
+					src: ['*.css', '!*.min.css'],
+					dest: 'css',
+					ext: '.min.css'
+				}]
+			}
 		}
+
 	}); // end of initConfig
+
+
+	// --------- Load Tasks ---------- //
 
 	// Page Speed Insights
 	grunt.loadNpmTasks('grunt-pagespeed');
+	
+	// CSS minify
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
+
+	// --------- Register/Run Tasks ---------- //
 
 	// 'grunt speed' in cmd line to run
 	grunt.registerTask('speed', ['pagespeed']);
 
 	// css grunt tasks
-	grunt.registerTask('default', ['pagespeed']);
+	grunt.registerTask('default', ['cssmin']);
+
 }; // end of module.exports
 
 
